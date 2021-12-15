8f9a5788893ca2a1ba55a0e9cd1324f45cb016b9
Harrison <HarrisonLeitch1@gmail.com>
2021-12-15 12:30:51 +0000

    Stuff done

--- Assets/PresentHoldPoint.cs
+++ Assets/PresentHoldPoint.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PresentHoldPoint : MonoBehaviour
 {
+    public Transform holdPoint;
+    public bool presentHeld;
+    public Present present;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (presentHeld)
+        {
+            present.transform.position = this.holdPoint.position;
+
+        }
+    }
+
+    public void presentNotHeld()
+    {
+        present = null;
+        presentHeld = false;
+    }
+    public void HoldPresent(Present present)
+    {
+        presentHeld = true;
+        this.present = present;
     }
 }
--- Assets/Scripts/GronchCollecting.cs
+++ Assets/Scripts/GronchCollecting.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class GronchCollecting : MonoBehaviour
 {
     bool spaceHit = false;
-
-
+    Present presentCollected;
+    PresentHoldPoint presentHolder;
     // Start is called before the first frame update
     void Start()
     {
-        
+        presentHolder = GetComponent<PresentHoldPoint>();
     }
 
     // Update is called once per frame
@@ -21,12 +21,25 @@ public class GronchCollecting : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Present")
+        if (collision.gameObject.tag == "Present")
         {
-            if(spaceHit)
+            if (spaceHit)
             {
                 Debug.Log("PresentCollected");
+
+
+                if (collision.TryGetComponent<Present>(out presentCollected))
+                {
+                    collision.enabled = false;
+                    presentHolder.HoldPresent(presentCollected);
+                }
             }
         }
     }
+    void PresentReset()
+    {
+        presentCollected = null;
+        presentHolder.presentNotHeld();
+    }
+
 }
